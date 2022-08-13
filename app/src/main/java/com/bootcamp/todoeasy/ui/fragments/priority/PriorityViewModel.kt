d06f7e15ec6f7f5659e6f226bd63aa5faf40727a
Gabriel Conti <67682500+gabrielcontimachado2@users.noreply.github.com>
2022-08-13 18:58:28 -0300

    Create Priority Dialog Fragment and Update the Priority in Activity Detail Task

---
+++ app/src/main/java/com/bootcamp/todoeasy/ui/fragments/priority/PriorityViewModel.kt
@@ -0,0 +1,20 @@
+package com.bootcamp.todoeasy.ui.fragments.priority
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bootcamp.todoeasy.domain.RepositoryImp
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class PriorityViewModel @Inject constructor(
+    private val repositoryImp: RepositoryImp
+) : ViewModel() {
+
+
+    fun updateTaskPriority(taskId: String, taskPriority: Int) = viewModelScope.launch {
+        repositoryImp.updateTaskPriority(taskId, taskPriority)
+    }
+
+}
